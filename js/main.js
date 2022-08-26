f5a4ab86ba80de890796c1ae60d05fa6c83b1644
Mariana Mostarda <mostarda.mariana@gmail.com>
2022-08-25 23:15:52 -0300

    Agregado de libreria

--- js/main.js
+++ js/main.js
@@ -1,42 +1,48 @@
+const cardSection = document.querySelector('#cardContainer');
+const botonTodos = document.querySelector('#btnTodos');
+const botonMesa = document.querySelector('#btnMesa');
+const botonSillas = document.querySelector('#btnSillas');
+const botonSofas = document.querySelector('#btnSofas');
+const botonDeco = document.querySelector('#btnDeco');
+const botonEscrit = document.querySelector('#btnEscrit');
+
 const mueblesDisponibles = [];
 
 class Mueble {
-    constructor(id, image, tipo, nombre, precio) {
+    constructor(id, image, tipo, nombre, precio, cantidad) {
         this.id = id
         this.img = image
         this.tipo = tipo
         this.nombre = nombre
         this.precio = precio
+        this.cantidad = cantidad
     }
 };
 
-const mueble1 = new Mueble(1, "mesa-alaia1.webp", "Mesa", "Alaia", 1690);
+const mueble1 = new Mueble(1, "mesa-alaia1.webp", "Mesa", "Alaia", 1690, 5);
 mueblesDisponibles.push(mueble1);
 
-const mueble2 = new Mueble(2, "mesa-anit1.webp", "Mesa", "Anit", 665);
+const mueble2 = new Mueble(2, "mesa-anit1.webp", "Mesa", "Anit", 665, 7);
 mueblesDisponibles.push(mueble2);
 
-const mueble3 = new Mueble(3, "sofa-brida1.webp", "Sofa", "Brida", 1415);
+const mueble3 = new Mueble(3, "sofa-brida1.webp", "Sofa", "Brida", 1415, 10);
 mueblesDisponibles.push(mueble3);
 
-const mueble4 = new Mueble(4, "silla-ralf1.webp", "Silla", "Ralf", 180);
+const mueble4 = new Mueble(4, "silla-ralf1.webp", "Silla", "Ralf", 180, 20);
 mueblesDisponibles.push(mueble4);
 
-const mueble5 = new Mueble(5, "silla-nayme1.webp", "Silla", "Nayme", 275);
+const mueble5 = new Mueble(5, "silla-nayme1.webp", "Silla", "Nayme", 275, 20);
 mueblesDisponibles.push(mueble5);
 
-const mueble6 = new Mueble(6, "cuadro-lorelai1.webp", "Cuadro", "Lorelai", 85);
+const mueble6 = new Mueble(6, "cuadro-lorelai1.webp", "Cuadro", "Lorelai", 85, 10);
 mueblesDisponibles.push(mueble6);
 
-const mueble7 = new Mueble(7, "escritorio-curie1.webp", "Escritorio", "Curie", 586);
+const mueble7 = new Mueble(7, "escritorio-curie1.webp", "Escritorio", "Curie", 586, 15);
 mueblesDisponibles.push(mueble7);
 
-const mueble8 = new Mueble(8, "sofa-gilma1.webp", "Sofa", "Gilma", 2135);
+const mueble8 = new Mueble(8, "sofa-gilma1.webp", "Sofa", "Gilma", 2135, 10);
 mueblesDisponibles.push(mueble8);
 
-
-
-const cardSection = document.querySelector('#cardContainer');
 
 mostrarProd(mueblesDisponibles);
 
@@ -50,28 +56,26 @@ function mostrarProd(array) {
             <h4>${producto.nombre}</h4>
             <img src="./assets/image/${producto.img}" alt="Imagen del Producto ${producto.nombre} en venta">
             <p>${producto.tipo}</p>
-            <p>U$S ${producto.precio}</p>
+            <div class="productBuy">
+                <p>U$S ${producto.precio}</p>
+                <button id="addToCart-btn" class="cart-btn" type="submit" onclick="addToCart(${producto.id})">Comprar</button>
+            <div>
         `;
         cardSection.appendChild(divCard);
     })
 };
 
 
-
 // Apretar boton y se filtra por tipo
 
-const botonTodos = document.querySelector('#btnTodos')
-
-botonTodos.addEventListener('click', mostrarTodo)
+botonTodos.addEventListener('click', mostrarTodo);
 
 function mostrarTodo() {
     cardSection.innerHTML = '';
     mostrarProd(mueblesDisponibles);
-}
+};
 
-const botonMesa = document.querySelector('#btnMesa')
-
-botonMesa.addEventListener('click', mostrarMesas)
+botonMesa.addEventListener('click', mostrarMesas);
 
 function mostrarMesas() {
     const mesasDisponibles = mueblesDisponibles.filter(elemento => {
@@ -79,11 +83,9 @@ function mostrarMesas() {
     });
     cardSection.innerHTML = '';
     mostrarProd(mesasDisponibles);
-}
+};
 
-const botonSillas = document.querySelector('#btnSillas')
-
-botonSillas.addEventListener('click', mostrarSillas)
+botonSillas.addEventListener('click', mostrarSillas);
 
 function mostrarSillas() {
     const sillasDisponibles = mueblesDisponibles.filter(elemento => {
@@ -91,12 +93,10 @@ function mostrarSillas() {
     });
     cardSection.innerHTML = '';
     mostrarProd(sillasDisponibles);
-}
+};
 
 
-const botonSofas = document.querySelector('#btnSofas')
-
-botonSofas.addEventListener('click', mostrarSofas)
+botonSofas.addEventListener('click', mostrarSofas);
 
 function mostrarSofas() {
     const sofasDisponibles = mueblesDisponibles.filter(elemento => {
@@ -104,12 +104,10 @@ function mostrarSofas() {
     });
     cardSection.innerHTML = '';
     mostrarProd(sofasDisponibles);
-}
+};
 
 
-const botonDeco = document.querySelector('#btnDeco')
-
-botonDeco.addEventListener('click', mostrarDeco)
+botonDeco.addEventListener('click', mostrarDeco);
 
 function mostrarDeco() {
     const decoDisponibles = mueblesDisponibles.filter(elemento => {
@@ -117,10 +115,8 @@ function mostrarDeco() {
     });
     cardSection.innerHTML = '';
     mostrarProd(decoDisponibles);
-}
+};
 
-
-const botonEscrit = document.querySelector('#btnEscrit')
 
 botonEscrit.addEventListener('click', mostrarEscrit)
 
@@ -130,4 +126,4 @@ function mostrarEscrit() {
     });
     cardSection.innerHTML = '';
     mostrarProd(escritDisponibles);
-}
+};
---
+++ js/shopCart.js
@@ -0,0 +1,160 @@
+// Carrito
+const cartListSection = document.querySelector('#cartSection');
+const cartListEmpty = document.querySelector('#cartEmpty');
+const cartListFull = document.querySelector('#cartList');
+const botonCarrito = document.querySelector('#btnShop');
+const botonCompra = document.querySelector('#cart-buy');
+const listProducts = document.querySelector('#list-allProducts');
+const totalProducts = document.querySelector('#totalProducts');
+const totalProdPrice = document.querySelector('#totalPrice');
+
+let cart = [];
+
+// Agregar articulos al carrito
+
+function addToCart(id) {
+    // revisar si el producto ya esta en el carrito
+    if(cart.some((item) => item.id === id)){
+        Swal.fire({
+            title: 'El producto ya está en el carrito!',
+            text: 'Haga click en el carrito para ajustar la cantidad',
+            icon: 'error',
+            confirmButtonText: 'Ok'
+        })
+    } else {
+        const item = mueblesDisponibles.find((mueble) => mueble.id === id);
+        cart.push({
+            ...item,
+            cantidadUnidades: 1,
+        });
+        // TOAST MESSAGE
+        Swal.fire({
+            title: '¡Se agregó el producto al carrito!',
+            icon: 'success',
+            position: 'bottom-right',
+            timer: 2000,
+            showConfirmButton: false,
+            toast: true
+        });
+    }
+
+    updateCart();
+};
+
+// Actualizar carrito
+
+function updateCart() {
+    renderCartItems();
+    renderTotal();
+}
+
+function renderCartItems() {
+    listProducts.innerHTML = "";
+
+    cart.forEach((item) => {
+        listProducts.innerHTML += `
+            <div class="list-product">
+                <p>${item.tipo} ${item.nombre}</p>
+                <p>U$S ${item.precio}</p>
+                <div class="counter">
+                    <div class="counter-btn" onclick="changeNumUnits('plus', ${item.id})">+</div>
+                    <div class="count">${item.cantidadUnidades}</div>
+                    <div class="counter-btn" onclick="changeNumUnits('minus', ${item.id})">-</div>
+                </div>
+                <p>Total</p>
+                <div>
+                    <button class="cart-btn" type="submit" onclick="removeCartItems(${item.id})">Quitar</button>
+                </div>
+            </div>
+        `
+    });
+};
+
+// Quitar items del carrito
+
+function removeCartItems(id) {
+    cart = cart.filter((item) => item.id !== id);
+
+    updateCart();
+};
+
+// Cambiar cantidad de items
+
+function changeNumUnits(action, id) {
+    cart = cart.map((item) => {
+        let cantidadUnidades = item.cantidadUnidades;
+
+        if(item.id === id) {
+            if(action === "plus" && cantidadUnidades < item.cantidad){
+                cantidadUnidades++;
+            }else if(action === "minus" && cantidadUnidades > 1){
+                cantidadUnidades--;
+            }
+        }
+        return {
+            ...item,
+            cantidadUnidades,
+        };
+    });
+
+    updateCart();
+};
+
+function renderTotal() {
+    let totalPrice = 0;
+    let totalItems = 0;
+
+    cart.forEach((item) => {
+        totalPrice += item.precio * item.cantidadUnidades;
+        totalItems += item.cantidadUnidades;
+    });
+    totalProducts.innerHTML = `${totalItems}`;
+    totalProdPrice.innerHTML = `U$S ${totalPrice}`;
+};
+
+// Boton carrito
+
+botonCarrito.addEventListener('click', showShoppingList);
+
+function showShoppingList() {
+    if(cart.length === 0){
+        cartListSection.classList.remove("hidden");
+        cartListEmpty.classList.remove("hidden");
+    } else if(cart.length >= 1){
+        cartListSection.classList.remove("hidden");
+        cartListEmpty.classList.add("hidden");
+        cartListFull.classList.remove("hidden");
+    }
+};
+
+// Mensajes de alerta boton comprar
+
+botonCompra.addEventListener('click', buyShoppingList);
+
+function buyShoppingList(){
+    if(cart.length >= 1){
+        Swal.fire({
+            title: '¡La compra se realizó con éxito!',
+            text: 'Muchas gracias',
+            icon: 'success',
+            confirmButtonText: 'Ok'
+        });
+    } else {
+        Swal.fire({
+            title: '¡El carrito está vacío!',
+            text: 'Elija los artículos a comprar',
+            icon: 'error',
+            confirmButtonText: 'Ok'
+        });
+    };
+    emptyCart()
+};
+
+function emptyCart(){
+    cart = [];
+    setTimeout(reloadPage, 3000);
+};
+
+function reloadPage(){
+    location.reload()
+};
